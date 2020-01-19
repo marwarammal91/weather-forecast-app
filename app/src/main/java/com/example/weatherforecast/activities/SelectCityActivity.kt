4307a9ece60cd248a5ac3c3309b449d460dbb231
Marwa Rammal <marwa.rammal@saradarbank.com>
2020-01-19 17:10:37 +0200

    fix: add multi-select city activity

---
+++ app/src/main/java/com/example/weatherforecast/activities/SelectCityActivity.kt
@@ -0,0 +1,13 @@
+package com.example.weatherforecast.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weatherforecast.R
+
+class SelectCityActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_city)
+    }
+}
