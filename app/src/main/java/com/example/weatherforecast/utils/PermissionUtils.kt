2836ff46a40c16d5f8c9b2155391b13231c632c7
Marwa Rammal <marwa.rammal@saradarbank.com>
2020-01-19 22:27:32 +0200

    fix: add permissionUtils to get current location

---
+++ app/src/main/java/com/example/weatherforecast/utils/PermissionUtils.kt
@@ -0,0 +1,30 @@
+package com.example.weatherforecast.utils
+
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import androidx.core.app.ActivityCompat
+
+object PermissionUtils {
+
+    fun isPermissionGranted(activity: Activity, permission: String): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.checkSelfPermission(activity, permission) == PackageManager.PERMISSION_GRANTED
+        } else {
+            true
+        }
+    }
+
+    fun openSettingsDialog(activity: Activity, message: String, requestCode: Int) {
+        Utils.showDialogActions(activity, message,
+            "Take Me To SETTINGS", "Cancel", {
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = Uri.fromParts("package", activity.packageName, null)
+                intent.data = uri
+                activity.startActivityForResult(intent, requestCode)
+            }, null)
+    }
+}
