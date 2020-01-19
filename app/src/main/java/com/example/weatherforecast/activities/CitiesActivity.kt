064e733c097f1e9cf85b040ae3794f0023136110
Marwa Rammal <marwa.rammal@saradarbank.com>
2020-01-19 11:19:02 +0200

    fix: add Cities activitry

---
+++ app/src/main/java/com/example/weatherforecast/activities/CitiesActivity.kt
@@ -0,0 +1,13 @@
+package com.example.weatherforecast.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weatherforecast.R
+
+class CitiesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cities)
+    }
+}
