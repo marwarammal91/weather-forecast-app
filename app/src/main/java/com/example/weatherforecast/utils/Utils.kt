66977b5fe44271e9cdf6c2bb61ffdbdee5e4e70d
Marwa Rammal <marwa.rammal@saradarbank.com>
2020-01-19 22:28:32 +0200

    chore: add custom alerts to utils

--- app/src/main/java/com/example/weatherforecast/utils/Utils.kt
+++ app/src/main/java/com/example/weatherforecast/utils/Utils.kt
@@ -1,6 +1,10 @@
 package com.example.weatherforecast.utils
 
+import android.app.Activity
+import android.app.AlertDialog
 import android.content.Context
+import android.location.LocationManager
+import android.os.Handler
 import java.io.InputStream
 
 object Utils {
@@ -16,4 +20,43 @@ object Utils {
         }
         return json
     }
+
+    fun isLocationEnabled(activity: Activity): Boolean {
+        val locationManager: LocationManager = activity.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    // region "alert dialogs"
+    fun showDialogActions(activity: Activity, msg: String, posAction: String, negAction: String, posRunnable: () -> Unit, negRunnable: (() -> Unit)?): AlertDialog? {
+        val diag = AlertDialog.Builder(activity)
+        diag.setMessage(msg)
+        diag.setCancelable(false)
+        diag.setPositiveButton(posAction) { dialog, _ ->
+            dialog.dismiss()
+            Handler().post(posRunnable)
+        }
+        diag.setNegativeButton(negAction) { dialog, _ ->
+            dialog.dismiss()
+            Handler().post(negRunnable)
+        }
+        return if (activity.isFinishing || activity.isDestroyed) null else diag.show()
+    }
+
+    fun showAlertDialog(activity: Activity, msg: String, action: String, finishActivity: Boolean) {
+        val diag = AlertDialog.Builder(activity)
+        diag.setMessage(msg)
+        diag.setCancelable(false)
+        diag.setPositiveButton(action) { dialog, _ ->
+            dialog.dismiss()
+            if (finishActivity) {
+                activity.finish()
+            }
+        }
+        if (!activity.isFinishing) {
+            diag.show()
+        }
+    }
+    // endregion
 }
