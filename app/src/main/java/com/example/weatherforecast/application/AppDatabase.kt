f1a258d5beef81817311439e1a143998150a9f08
Marwa Rammal <marwa.rammal@saradarbank.com>
2020-01-19 12:11:35 +0200

    fix: add appdatabse using Room

---
+++ app/src/main/java/com/example/weatherforecast/application/AppDatabase.kt
@@ -0,0 +1,26 @@
+package com.example.weatherforecast.application
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.weatherforecast.models.City
+import com.example.weatherforecast.models.CityDao
+
+@Database(entities = [City::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun cityDao(): CityDao
+
+    companion object {
+        @Volatile private var instance: AppDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context)= instance ?: synchronized(LOCK){
+            instance ?: buildDatabase(context).also { instance = it}
+        }
+
+        private fun buildDatabase(context: Context) = Room.databaseBuilder(context,
+            AppDatabase::class.java, "app.db")
+            .build()
+    }
+}
