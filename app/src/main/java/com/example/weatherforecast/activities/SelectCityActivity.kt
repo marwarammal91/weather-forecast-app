a2e23d0df43ccee09b88ca6643d30f37cbfb1952
Marwa Rammal <marwa.rammal@saradarbank.com>
2020-01-19 19:35:59 +0200

    fix: implement multi select activity for citites

--- app/src/main/java/com/example/weatherforecast/activities/SelectCityActivity.kt
+++ app/src/main/java/com/example/weatherforecast/activities/SelectCityActivity.kt
@@ -1,13 +1,100 @@
 package com.example.weatherforecast.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.os.AsyncTask
 import android.os.Bundle
+import android.view.View.VISIBLE
+import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.weatherforecast.R
+import com.example.weatherforecast.adapters.CityAdapter
+import com.example.weatherforecast.application.App
+import com.example.weatherforecast.models.City
+import com.example.weatherforecast.models.CityDao
+import kotlinx.android.synthetic.main.activity_select_city.*
+import kotlinx.android.synthetic.main.layout_header.*
+
 
 class SelectCityActivity : AppCompatActivity() {
+
+    private lateinit var cityAdapter: CityAdapter
+    private lateinit var cityDao: CityDao
+
+    var searchQuery = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_city)
+
+        cityDao = App.appDatabase.cityDao()
+
+        // header view
+        titleTextView.text = getString(R.string.select_city)
+        backBtn.setOnClickListener {
+            finish()
+        }
+        saveTextView.visibility = VISIBLE
+        saveTextView.setOnClickListener{
+            save()
+        }
+
+        val llManager = LinearLayoutManager(this)
+        rvMultiSelect.layoutManager = llManager
+
+        // get selected cities
+        val selectedItems = cityDao.getAllFavoriteCities()
+        val cityLists = cityDao.getAll()
+        cityAdapter = CityAdapter(ArrayList(cityLists), ArrayList(selectedItems))
+        rvMultiSelect.adapter = cityAdapter
+
+        // search view
+        citySearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                if (query.isNotEmpty() && query != searchQuery) {
+                    searchQuery = query
+                    generateList(query)
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                if (newText.isNotEmpty() && newText != searchQuery) {
+                    searchQuery = newText
+                    generateList(newText)
+                }
+                return false
+            }
+        })
+    }
+
+    private fun generateList(search: String) {
+        var filteredDataList = cityDao.findByNameCountry(search, search)
+        if (search.isNotEmpty()) {
+            filteredDataList = cityDao.getAll()
+        }
+
+        val size = cityAdapter.cityListItems.size
+        cityAdapter.cityListItems.clear()
+        cityAdapter.notifyItemRangeRemoved(0, size)
+        cityAdapter.updateList(ArrayList(filteredDataList))
+        cityAdapter.notifyItemInserted(filteredDataList.size)
+        cityAdapter.notifyDataSetChanged()
+    }
+
+    private fun save() {
+        val arrayOfSelectedItems = ArrayList<City>()
+        for (i in 0 until cityAdapter.itemCount) {
+            if (cityAdapter.getItem(i).isFavorite) {
+                val updateCity = cityAdapter.getItem(i)
+                cityDao.updateCity(updateCity.isFavorite, updateCity.cityId)
+                arrayOfSelectedItems.add(cityAdapter.getItem(i))
+            }
+        }
+        val output = Intent()
+        output.putExtra("result", arrayOfSelectedItems)
+        setResult(Activity.RESULT_OK, output)
+        finish()
     }
 }
---
+++ app/src/main/java/com/example/weatherforecast/adapters/CityAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.weatherforecast.adapters
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weatherforecast.R
+import com.example.weatherforecast.models.City
+
+
+class CityAdapter(var cityListItems: ArrayList<City>,
+                  var selectedItems: ArrayList<City>) :
+    RecyclerView.Adapter<CityAdapter.CityMultiSelectHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CityMultiSelectHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.multiselect_row_item, parent, false)
+        return CityMultiSelectHolder(view)
+    }
+
+    fun updateList(cityList: ArrayList<City>) {
+        this.cityListItems = cityList
+    }
+
+    fun getItem(position: Int): City = cityListItems[position]
+
+    override fun getItemCount(): Int = cityListItems.size
+
+    override fun getItemId(position: Int): Long = position.toLong()
+
+    override fun getItemViewType(position: Int): Int = position
+
+    override fun onBindViewHolder(holder: CityMultiSelectHolder, position: Int) {
+        val field = cityListItems[position]
+        holder.bind(field)
+    }
+
+    inner class CityMultiSelectHolder internal constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private var checkBox = itemView.findViewById<CheckBox>(R.id.multiSelectItem)
+
+        @SuppressLint("SetTextI18n")
+        fun bind(field: City) {
+            checkBox.text = "Country:" + field.country + " - City:" + field.name
+            if (selectedItems.any { it.name == field.name }) {
+                checkBox.isChecked = true
+                field.isFavorite = true
+            }
+            checkBox!!.setOnClickListener {
+                field.isFavorite = checkBox.isChecked
+            }
+        }
+    }
+}
