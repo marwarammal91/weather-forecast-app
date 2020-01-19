cfd593eadc838d906312b43f906092e1ae1daf06
Marwa Rammal <marwa.rammal@saradarbank.com>
2020-01-19 16:35:45 +0200

    fix: add inforDialog alert to the citiesactivity

--- app/src/main/java/com/example/weatherforecast/activities/CitiesActivity.kt
+++ app/src/main/java/com/example/weatherforecast/activities/CitiesActivity.kt
@@ -2,12 +2,49 @@ package com.example.weatherforecast.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.FragmentActivity
 import com.example.weatherforecast.R
+import com.example.weatherforecast.fragments.InfoDialog
+import kotlinx.android.synthetic.main.activity_cities.*
 
 class CitiesActivity : AppCompatActivity() {
+
+    var inforDialogFragment: InfoDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cities)
+
+
+        // set click listeners
+        addCityBtn.setOnClickListener(onClickListers)
+        infoBtn.setOnClickListener(onClickListers)
+
+
+        showInfoDialog()
+    }
+
+    private val onClickListers =  View.OnClickListener { view ->
+        when (view.id) {
+            R.id.addCityBtn -> {
+
+            }
+            R.id.infoBtn -> {
+                showInfoDialog()
+            }
+        }
+    }
+
+    fun showInfoDialog() {
+        if (!isFinishing) {
+            inforDialogFragment = InfoDialog.newInstance()
+            inforDialogFragment!!.show(supportFragmentManager, "inforDialog")
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        inforDialogFragment?.dismissAllowingStateLoss()
     }
 }
